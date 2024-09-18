dca3f8a1afdfe758e9d6914acd5ccddcf83d59ac
Nguyen khoi <142767730+masakechan@users.noreply.github.com>
2024-09-18 15:46:35 +0700

    cat dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error"); // Thêm phần import ApiError
+const contactsRouter = require("./app/routes/contact.route"); // Thêm phần import contactsRouter
 
 const app = express();
 
@@ -7,14 +9,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Đăng ký các routes
+app.use("/api/contacts", contactsRouter);
+
 // Route mặc định
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
 
-// Import contactsRouter và đăng ký routes
-const contactsRouter = require("./app/routes/contact.route");
+// Xử lý response 404
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
 
-app.use("/api/contacts", contactsRouter);
+// Xử lý lỗi tập trung
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error", // Sửa lỗi biến `error` thành `err`
+    });
+});
 
 module.exports = app;
